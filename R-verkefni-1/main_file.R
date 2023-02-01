925f1a74f3121f724de68b33c4347203bc837c7a
hjoddi <hjoddi@gmail.com>
2023-02-01 19:47:25 +0000

    1a done

--- R-verkefni-1/main_file.R
+++ R-verkefni-1/main_file.R
@@ -4,5 +4,16 @@
 # Load packages.
 library(datasets)
 
-# Load data.
-HS = read.csv("Data/husnaedisverd_2017_21.csv")
+# Load data. [1a]
+HS = read.csv("Data/husnaedisverd_2017_21.csv", sep = ";", header = TRUE)
+
+# Preliminary data exploration of full dataset.
+head(HS)
+summary(HS)
+
+# Narrowing of dataset to three neighbourhoods.[1b]
+hverfi <- c(500, 170, 300)
+HS <- filter(HS, matssvaedi%in%hverfi)
+
+# Preliminary data exploration of narrowed dataset.
+head(HS)
