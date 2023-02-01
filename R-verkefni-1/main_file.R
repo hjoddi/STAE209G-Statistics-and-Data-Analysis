860e69ca8eba8a2ae5ddcb2584925a57529cc424
hjoddi <hjoddi@gmail.com>
2023-02-01 19:46:56 +0000

    1b done

---
+++ R-verkefni-1/main_file.R
@@ -0,0 +1,8 @@
+# TODO: Description
+
+# Install packages if necessary.
+# Load packages.
+library(datasets)
+
+# Load data.
+HS = read.csv("Data/husnaedisverd_2017_21.csv")
---
+++ R-verkefni-1/packages.R
@@ -0,0 +1,19 @@
+# Name, etc.
+
+# Install packages
+install.packages("pacman")
+
+# Load packages
+library(datasets)
+
+## Load packages through pacman.
+library(pacman)
+pacman::p_load(pacman, dplyr, GGally, ggplot2, ggthemes, ggvis,
+               httr, plotly, rio, rmarkdown, shiny, stringr, tidyr)
+
+# Clear packages
+p_unload(all) # Pacman packages.
+detach("package:datasets", unload = TRUE) # Base packages.
+
+# Clear console.
+cat("\014") # ctrl+L
